c3ebdaaab5bce21dec3965afbcdf45efa50bd2c6
Jonathan <jonmd@hotmail.com>
2018-12-05 19:13:18 -0600

    fix client

--- SLADE/Sensor_Move_Arm/XB_Client.py
+++ SLADE/Sensor_Move_Arm/XB_Client.py
@@ -3,12 +3,21 @@ import sys
 from PI_XBSerCli import *
 
 
-IP_ADDR = raw_input("Input IP Address: ")
-PORT = input("Input Port Number: ")
-COM_ARD = raw_input("Input Arduino Serial Port: ")
-BAUD_RATE_ARD = input("Input Arduino Baud Rate: ")
-COM_XB = raw_input("Input XBee Serial Port: ")
-BAUD_RATE_XB = input("Input XBee Baud Rate: ")
+if sys.version_info[0] == 3:
+    IP_ADDR = input("Input IP Address: ")
+    PORT = input("Input Port Number: ")
+    COM_ARD = input("Input Arduino Serial Port: ")
+    BAUD_RATE_ARD = input("Input Arduino Baud Rate: ")
+    COM_XB = input("Input XBee Serial Port: ")
+    BAUD_RATE_XB = input("Input XBee Baud Rate: ")
+else:
+    IP_ADDR = raw_input("Input IP Address: ")
+    PORT = input("Input Port Number: ")
+    COM_ARD = raw_input("Input Arduino Serial Port: ")
+    BAUD_RATE_ARD = input("Input Arduino Baud Rate: ")
+    COM_XB = raw_input("Input XBee Serial Port: ")
+    BAUD_RATE_XB = input("Input XBee Baud Rate: ")
+
 
 xb_serial_client = PI_XBSerCli(IP_ADDR, PORT, COM_ARD, BAUD_RATE_ARD, COM_XB, BAUD_RATE_XB)
 
