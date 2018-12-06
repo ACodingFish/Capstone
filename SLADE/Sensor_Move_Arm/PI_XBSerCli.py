f4ba55a0e2d66ac1aa007ac18a6298d5d832ff19
Jonathan <jonmd@hotmail.com>
2018-12-05 19:34:52 -0600

    fix

--- SLADE/Sensor_Move_Arm/PI_XBSerCli.py
+++ SLADE/Sensor_Move_Arm/PI_XBSerCli.py
@@ -64,8 +64,7 @@ class PI_XBSerCli:
         while True:
             time.sleep(self.xb_sleep_time)
             data = self.xb_comm.get_msg()
-            print(data)
-            average = int(data)
+            average = data
             if (average <= self.xb_min_dist):
                 self.serial_write(self.GO_HOME)
                 self.Send_Msg("Robot Went Home")
