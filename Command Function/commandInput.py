9f2374109caee8379249c7aad52a519b27987db7
Delsaaga <44508236+Delsaaga@users.noreply.github.com>
2019-01-28 14:46:41 -0600

    Update commandInput.py
    Optimization code

--- Command Function/commandInput.py
+++ Command Function/commandInput.py
@@ -1,43 +1,15 @@
 
-import sys
 
-# allows code in python 2 AND 3 
-if sys.version_info[0] == 3:
-    from _thread import *
-else:
-    from thread import *
-        
-angle = ""
-servo = ""
-        
-def parse_function(command_in):
-    
-    global angle
-    global servo
-    j = 0
-    temp = ""
-    
-    for i in command_in:
-        sorter = list(command_in[j])
-        #print(sorter)
-        m = 0
-        
-        for k in sorter:
-            number_indicator = sorter[m].isdigit()
-            
-            if number_indicator == True:
-                temp = sorter[m]
-                angle = angle+temp
-                m = m+1
+def parse(command_in):
+
+    for command in command_in.split(", "):
+
+        index = 0
+        for number in command:
+            if (number.isdigit()): 
+                index += 1
+            elif (index>0):
+                print("\tIndex: ",int(command[:index]),"\tString: ",command[index:])
+                break
                 
-            if number_indicator == False:
-                servo = sorter[m]
-                m = m+1
-                print(angle, servo)
-                angle = ""
-                servo = ""
-        j = j+1
-            
-   
-
-parse_function("12a 14b")
+parse("12a, 120b, 80crap, Gin, and, Rum")
