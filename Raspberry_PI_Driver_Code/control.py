4d2dd1f3f468d7cb49fa398b9b2596c79164a9b7
Delsaaga <jczoodsma21@students.tntech.edu>
2019-01-29 14:14:13 -0600

    add existing file

---
+++ Raspberry_PI_Driver_Code/control.py
@@ -0,0 +1,11 @@
+import os
+from PI_Servo import *
+from commandInput import *
+
+servo_controller = PI_ServoController(16)
+while True:
+    key_msg = sys.stdin.readline()
+    if (key_msg[:4].lower() == "exit"):
+        os._exit(0)
+    parse(key_msg, servo_controller)
+    
