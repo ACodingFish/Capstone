8443ed8cf4d0ebb77254e6c0f525d20fc907bd0e
TheDemonfish <jonmd@hotmail.com>
2019-02-12 23:49:46 -0600

    new fix

--- commit_script.sh
+++ commit_script.sh
@@ -6,9 +6,7 @@ git config --global user.email "$u_email"
 read -p "Enter Commit Msg: " send_msg
 git add .
 git commit -m "$send_msg"
-git push http://github.com/TheDemonfish/Capstone master <<'EOF'
-$u_name
-EOF
+( sleep 10 ; printf "$u_name\n" ) | git push http://github.com/TheDemonfish/Capstone master
 
 wait ${!}
 echo "done"$'\r'
