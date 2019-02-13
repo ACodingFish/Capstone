4becfcd60bd8830320eab33372babcd9070ebaee
TheDemonfish <jonmd@hotmail.com>
2019-02-12 23:08:52 -0600

    fixes to scripts

--- gitconfig.sh
+++ gitconfig.sh
@@ -4,3 +4,6 @@ read -p "Enter Username: " u_name
 git config --global user.name "$u_name"
 read -p "Enter Email: " u_email
 git config --global user.email "$u_email"
+wait ${!}
+echo "done"
+sleep 10
--- pull_script.sh
+++ pull_script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 git pull http://github.com/TheDemonfish/Capstone
 wait ${!}
+echo "done"
+sleep 10
