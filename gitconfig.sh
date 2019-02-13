1a2b5711186859954fdea95205eee3f6ad86f4d2
TheDemonfish <jonmd@hotmail.com>
2019-02-13 00:08:36 -0600

    new fix

--- gitconfig.sh
+++ gitconfig.sh
@@ -4,6 +4,7 @@ read -p "Enter Username: " u_name
 git config --global user.name "$u_name"
 read -p "Enter Email: " u_email
 git config --global user.email "$u_email"
+git push --set-upstream http://github.com/TheDemonfish/Capstone master
 wait ${!}
 echo "done"
 sleep 10
