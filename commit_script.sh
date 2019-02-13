673522532561dc0d86ace3945fd5b9f0c1a07d18
TheDemonfish <jonmd@hotmail.com>
2019-02-12 22:59:39 -0600

    commit

--- commit_script.sh
+++ commit_script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
+read -p "Enter Username: " u_name
+git config --global user.name "$u_name"
+read -p "Enter Email: " u_email
+git config --global user.email "$u_email"
 read -p "Enter Commit Msg: " send_msg
 git add .
 git commit -m "$send_msg"
-git push http://github.com/TheDemonfish/Capstone master
+echo "$u_name" | git push http://github.com/TheDemonfish/Capstone master
 wait ${!}
