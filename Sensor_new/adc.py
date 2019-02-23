88d5d15b9e75849f17dbb05e5d2a75fc3adb044c
Jonathan <jonmd@hotmail.com>
2019-02-23 14:44:08 -0600

    slade adc code

--- Sensor_new/adc.py
+++ Sensor_new/adc.py
@@ -1,33 +1,67 @@
-#https://pypi.org/project/mcp3008/
-#pip install mcp3008
-#or 
-#https://learn.adafruit.com/mcp3008-spi-adc/python-circuitpython
-#sudo pip3 install adafruit-circuitpython-mcp3xxx
-#
-#pin 7 - CE1#
-#Pin 8 - CE0#
-#pin 9 - MISO
-#pin10 - MOSI
-#pin11 - SCLK
+import time
+import os
+import RPi.GPIO as GPIO
 
-import busio
-import digitalio
-import board
-import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
+GPIO.setmode(GPIO.BCM)
+DEBUG = True
 
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-cs = digitalio.DigitalInOut(board.D5)
-mcp = MCP.MCP3008(spi, cs)
+# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+def readadc(channel, clockpin, mosipin, misopin, cspin):
+        if ((channel > 7) or (channel < 0)):
+                return -1
+        GPIO.output(cspin, True)
 
-num_adcs = 7
-analog_pins = [MCP.P0,MCP.P1,MCP.P2,MCP.P3,MCP.P4,MCP.P5,MCP.P6,MCP.P7]
-channels = []
-for i in range(len(analog_pins)):
-    channels[i] = AnalogIn(mcp, analog_pins[i])
-    
+        GPIO.output(clockpin, False)  # start clock low
+        GPIO.output(cspin, False)     # bring CS low
+
+        commandout = channel
+        commandout |= 0x18  # start bit + single-ended bit
+        commandout <<= 3    # we only need to send 5 bits here
+        for i in range(5):
+                if (commandout & 0x80):
+                        GPIO.output(mosipin, True)
+                else:
+                        GPIO.output(mosipin, False)
+                commandout <<= 1
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+
+        adcout = 0
+        # read in one empty bit, one null bit and 10 ADC bits
+        for i in range(12):
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+                adcout <<= 1
+                if (GPIO.input(misopin)):
+                        adcout |= 0x1
+
+        GPIO.output(cspin, True)
+        
+        adcout >>= 1       # first bit is 'null' so drop it
+        return adcout
+
+# SPI ports on the ADC to the Cobbler
+SPICLK = 11
+SPIMISO = 9
+SPIMOSI = 10
+SPICS = 8
+
+# set up the SPI interface pins
+GPIO.setup(SPIMOSI, GPIO.OUT)
+GPIO.setup(SPIMISO, GPIO.IN)
+GPIO.setup(SPICLK, GPIO.OUT)
+GPIO.setup(SPICS, GPIO.OUT)
+
 while True:
-    for i in range(len(channels)):
-        print("Val: ", channel.value)
-        print("Volt: ", str(channel.voltage))
-        sleep(0.5)
+        # we'll assume that the fsr hasn't been excited
+        for i in range(num_channels)
+        ################### Start sensor 1 code #########################
+        current_read1 = readadc(i, SPICLK, SPIMOSI, SPIMISO, SPICS)    # read the analog pin
+
+        if DEBUG:
+                print ("Sensor",i)
+                print ('Current Read:', current_read1)
+       
+        # hang out and do nothing for a half second
+        time.sleep(0.5)
+        os.system('clear')
