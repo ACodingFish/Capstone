2ab5e547f381b0fd7d06ad02c7abf3426cf72d60
Jonathan <jonmd@hotmail.com>
2019-03-03 15:16:24 -0600

    fixes and additions

---
+++ Revised_Code/Server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+#Server program
+import sys
+import os
+from PI_Srvr import *
+
+num_args = len(sys.argv)
+if (num_args >2):
+    print ("Too many command line args.")
+    exit();
+
+port = 10001
+
+if (num_args == 2):
+    try:
+        port = int(sys.argv[1])
+    except:
+        pass
+   
+srvr = PI_Srvr(port)
+
+while True:
+    key_msg = sys.stdin.readline()
+    if (key_msg[:4].lower() == "exit"):
+        os._exit(0)
