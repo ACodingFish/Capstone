08e40f60ebe25f20a8d0f26f00db892aaaa5f391
Delsaaga <44508236+Delsaaga@users.noreply.github.com>
2019-03-26 14:28:34 -0500

    client manager
    client manager code to separate clients.

---
+++ DL_Team/client_manager.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import time
+
+class PI_Client:
+    def __init__(self, name, idnum):
+        self.name = name
+        self.idnum = idnum
+        
+class PI_Client_Monitor:
+    def __init__(self):
+        self.client_list = []
+        
+    def add_client(self, name, idnum):
+        new_client = self.PI_Client(name, idnum)
+        self.client_list.append(new_client)
+        
+    def delete_client(self, idnum):
+        for i in range(0, len(self.client_list)):
+            if self.client_list[i].idnum == idnum:
+                pop(self.client_list[i])
+    
+    def search_client(self, name):
+        cli_arr = []
+        for client in self.client_list:
+            if client.name == name:
+                cli_arr.append(client.idnum)
+        return cli_arr
